043de765f7d827378005ca7a2a41a924b22e7e72
Abhi <moreabhi29@gmail.com>
2023-10-13 12:07:25 +0530

    RM

---
+++ src/components/about/About.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+import Back from "../common/Back"
+import Heading from "../common/Heading"
+import img from "../images/about.jpg"
+import "./about.css"
+
+const About = () => {
+  return (
+    <>
+      <section className='about'>
+        <Back name='About Us' title='About Us - Who We Are?' cover={img} />
+        <div className='container flex mtop'>
+          <div className='left row'>
+            <Heading title='Our Agency Story' subtitle='Check out our company story and work process' />
+
+            <p>Explore our latest additions to the real estate market, showcasing a diverse range of properties just listed. </p>
+            <p>Don't miss the opportunity to discover your dream property among our recently listed options </p>
+            <button className='btn2'>More About Us</button>
+          </div>
+          <div className='right row'>
+            <img src='./immio.jpg' alt='' />
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default About
---
+++ src/components/data/Data.js
@@ -0,0 +1,318 @@
+export const nav = [
+  {
+    text: "home",
+    path: "/",
+  },
+  {
+    text: "about",
+    path: "/about",
+  },
+  {
+    text: "services",
+    path: "/services",
+  },
+  {
+    text: "blog",
+    path: "/blog",
+  },
+  {
+    text: "pricing",
+    path: "/pricing",
+  },
+  {
+    text: "contact",
+    path: "/contact",
+  },
+]
+export const featured = [
+  {
+    cover: "../images/hero/h1.png",
+    name: "Family House",
+    total: "172 Property",
+  },
+  {
+    cover: "../images/hero/h2.png",
+    name: "House & Villa",
+    total: "159 Property",
+  },
+  {
+    cover: "../images/hero/h3.png",
+    name: "Apartment",
+    total: "317 Property",
+  },
+  {
+    cover: "../images/hero/h4.png",
+    name: "Office & Studio",
+    total: "83 Property",
+  },
+  {
+    cover: "../images/hero/h6.png",
+    name: "Villa & Condo",
+    total: "82 Property",
+  },
+]
+export const list = [
+  {
+    id: 1,
+    cover: "../images/list/p-1.png",
+    name: "Red Carpet Real Estate",
+    location: "210 Koregaon Park, pune",
+    category: "For Rent",
+    price: "₹ 37,000",
+    type: "Apartment",
+  },
+  {
+    id: 2,
+    cover: "../images/list/p-2.png",
+    name: "Fairmount Properties",
+    location: "5698 Koregaon Park, Pune",
+    category: "For Sale",
+    price: "₹ 1,60,00,000",
+    type: "Flat",
+  },
+  {
+    id: 3,
+    cover: "../images/list/p-7.png",
+    name: "The Real Estate Corner",
+    location: "5624 Mooker Market, Bengaluru",
+    category: "For Rent",
+    price: "₹ 28,000",
+    type: "Offices",
+  },
+  {
+    id: 4,
+    cover: "../images/list/p-4.png",
+    name: "Herringbone Realty",
+    location: "5621 Liverpool, Goa",
+    category: "For Sale",
+    price: "₹ 2,80,32,000",
+    type: "Homes & Villas",
+  },
+  {
+    id: 5,
+    cover: "../images/list/p-5.png",
+    name: "Brick Lane Realty",
+    location: "210 Montreal Road, Delhi",
+    category: "For Rent",
+    price: "₹34,000",
+    type: "Commercial",
+  },
+  {
+    id: 6,
+    cover: "../images/list/p-6.png",
+    name: "Banyon Tree Realty",
+    location: "210 Zirak Road, Mumbai",
+    category: "For Sale",
+    price: "₹3,98,09,000",
+    type: "Apartment",
+  },
+]
+export const awards = [
+  {
+    icon: <i class='fa-solid fa-trophy'></i>,
+    num: "32 	",
+    name: "Blue Burmin Award",
+  },
+  {
+    icon: <i class='fa-solid fa-briefcase'></i>,
+    num: "43 ",
+    name: "Mimo X11 Award",
+  },
+  {
+    icon: <i class='fa-solid fa-lightbulb'></i>,
+    num: "51 ",
+    name: " UGC Award",
+  },
+  {
+    icon: <i class='fa-solid fa-heart'></i>,
+    num: "42 ",
+    name: "IITCA Green Award",
+  },
+]
+export const location = [
+  {
+    id: 1,
+    name: "New Orleans, Pune",
+    Villas: "32 Villas",
+    Apartments: "65 Apartments",
+    Offices: "07 Offices",
+    cover: "./images/location/city-1.png",
+  },
+  {
+    id: 2,
+    name: "Jerrsy, Mumbai",
+    Villas: "49 Villas",
+    Apartments: "29 Apartments",
+    Offices: "13 Offices",
+    cover: "./images/location/city-2.png",
+  },
+  {
+    id: 3,
+    name: "Liverpool, Goa",
+    Villas: "37 Villas",
+    Apartments: " 17 Apartments",
+    Offices: "14 Offices",
+    cover: "./images/location/city-3.png",
+  },
+  {
+    id: 4,
+    name: " Dubai",
+    Villas: "08 Villas",
+    Apartments: " 13 Apartments",
+    Offices: "04 Offices",
+    cover: "./images/location/city-4.png",
+  },
+  {
+    id: 5,
+    name: "Montreal, Bengaluru",
+    Villas: "23 Villas",
+    Apartments: " 59 Apartments",
+    Offices: "23 Offices",
+    cover: "./images/location/city-5.png",
+  },
+  {
+    id: 6,
+    name: " Delhi",
+    Villas: "12 Villas",
+    Apartments: " 34 Apartments",
+    Offices: "19 Offices",
+    cover: "./images/location/city-6.png",
+  },
+]
+export const team = [
+  {
+    list: "34",
+    cover: "../images/customer/team-1.jpg",
+    address: "Liverpool, Goa",
+    name: "Sargam S. Singh",
+    icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+  {
+    list: "70",
+    cover: "../images/customer/team-2.jpg",
+    address: "Montreal, Bengaluru",
+    name: "Harijeet M. Siller",
+    icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+  {
+    list: "80",
+    cover: "../images/customer/team-3.jpg",
+    address: "Denever, Pune",
+    name: " Vaibhav Bugad ",
+    icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+  {
+    list: "51",
+    cover: "../images/customer/team-4.jpg",
+    address: "2272 Briarwood Mumbai",
+    name: "Manish Dev",
+    icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+  {
+    list: "42",
+    cover: "../images/customer/team-5.jpg",
+    address: "2272 Dubai",
+    name: "Michael P. Grimaldo",
+    icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+  {
+    list: "38",
+    cover: "../images/customer/team-6.jpg",
+    address: "Montreal, Delhi",
+    name: "Ram Patel",
+    icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+]
+export const price = [
+  {
+    plan: "Basic",
+    price: "29",
+    ptext: "per user, per month",
+    list: [
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "99.5% Uptime Guarantee",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "120GB CDN Bandwidth",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "5GB Cloud Storage",
+      },
+      { change: "color", icon: <i class='fa-solid fa-x'></i>, text: "Personal Help Support" },
+      { change: "color", icon: <i class='fa-solid fa-x'></i>, text: "Enterprise SLA" },
+    ],
+  },
+  {
+    best: "Best Value",
+    plan: "Standard",
+    price: "49",
+    ptext: "per user, per month",
+    list: [
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "99.5% Uptime Guarantee",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "150GB CDN Bandwidth",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "10GB Cloud Storage",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "Personal Help Support",
+      },
+      {
+        change: "color",
+        icon: <i class='fa-solid fa-x'></i>,
+        text: "Enterprise SLA",
+      },
+    ],
+  },
+  {
+    plan: "Platinum",
+    price: "79",
+    ptext: "2 user, per month",
+    list: [
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "100% Uptime Guarantee",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "200GB CDN Bandwidth",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "20GB Cloud Storage",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "Personal Help Support",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "Enterprise SLA",
+      },
+    ],
+  },
+]
+export const footer = [
+  {
+    title: "LAYOUTS",
+    text: [{ list: "Home Page" }, { list: "About Page" }, { list: "Service Page" }, { list: "Property Page" }, { list: "Contact Page" }, { list: "Single Blog" }],
+  },
+  {
+    title: "ALL SECTIONS",
+    text: [{ list: "Headers" }, { list: "Features" }, { list: "Attractive" }, { list: "Testimonials" }, { list: "Videos" }, { list: "Footers" }],
+  },
+  {
+    title: "COMPANY",
+    text: [{ list: "About" }, { list: "Blog" }, { list: "Pricing" }, { list: "Affiliate" }, { list: "Login" }, { list: "Changelog" }],
+  },
+]
